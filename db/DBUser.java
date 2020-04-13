488d3ab9d3dc139a4636033f688e0e75916b15f5
lamine2000 <lemzogueye.lg72@gmail.com>
2020-04-13 22:44:33 +0000

    Amelioration de user/DBUser.java en se servant de la classe DBConnect

--- db/DBUser.java
+++ db/DBUser.java
@@ -1,54 +1,11 @@
-package db;
-import java.io.FileInputStream;
-import java.util.Properties;
+//package db;
+
+import db.DBConnect;
 import java.sql.*;
 
 //ici sont definies l'ensemble des fonctions qui permettent aux utilisateurs de communiquer avec la base de données
 
 public class DBUser{
-
-	public static String[] chargerProprietes(String fichierProperties){
-		//charge le fichier de proprietes et renvoie ces dernieres dans un tableau de chaines de caracteres
-
-		Properties prop = null;
-		FileInputStream in = null;
-		String url, user, password, driver, proprietes;
-
-		try{
-			prop = new Properties();
-			in = new FileInputStream(fichierProperties);
-			prop.load(in);
-			in.close();
-		} catch(Exception e) {
-			System.out.println("Echec de chargement du fichier jdbc.properties");
-		}
-
-		url = prop.getProperty("jdbc.url");
-		user = prop.getProperty("jdbc.user");
-		password = prop.getProperty("jdbc.password");
-		driver = prop.getProperty("jdbc.driver");
-		proprietes = url+"°"+user+"°"+password+"°"+driver;
-		return proprietes.split("°");
-	}
-
-
-
-	public static Connection DBconnect(String[] tab){
-		//se sert des parametres de connexion pour se connecter a la base de donnees et
-		//renvoie l'objet Connection obtenu
-		
-		Connection conn = null;
-		
-		try{
-			Class.forName(tab[3]);
-			conn = DriverManager.getConnection(tab[0], tab[1], tab[2]);
-		} catch(Exception e){
-			System.out.println("Echec de connection a la base de donnees");
-		}
-		return conn;
-	}
-
-
 
 	public static int DBrechercherUser(String log, String pass){
 		
@@ -58,8 +15,7 @@ public class DBUser{
 		ResultSet result1 = null;
 		ResultSet result2 = null;
 
-		String[] tabProprietes = chargerProprietes("jdbc.properties");
-		Connection conn = DBconnect(tabProprietes);
+		Connection conn = DBConnect.getInstance().getConn();
 		
 		try{
 			state1 = conn.createStatement();
@@ -97,8 +53,7 @@ public class DBUser{
 		Statement state = null;
 		ResultSet result = null;
 
-		String[] tabProprietes = chargerProprietes("jdbc.properties");
-		Connection conn = DBconnect(tabProprietes);
+		Connection conn = DBConnect.getInstance().getConn();
 		
 		try{
 			state = conn.createStatement();
@@ -129,8 +84,7 @@ public class DBUser{
 		Statement state = null;
 		ResultSet result = null;
 
-		String[] tabProprietes = chargerProprietes("jdbc.properties");
-		Connection conn = DBconnect(tabProprietes);
+		Connection conn = DBConnect.getInstance().getConn();
 		
 		try{
 			state = conn.createStatement();
@@ -168,8 +122,7 @@ public class DBUser{
 		Statement state = null;
 		ResultSet result = null;
 
-		String[] tabProprietes = chargerProprietes("jdbc.properties");
-		Connection conn = DBconnect(tabProprietes);
+		Connection conn = DBConnect.getInstance().getConn();
 		
 		try{
 			state = conn.createStatement();
@@ -197,5 +150,10 @@ public class DBUser{
 		finally{
 			return liste;
 		}
+	}
+
+	public static void main(String[] args) {
+		int a = DBrechercherUser("lmzo", "qsdfjklm");
+		System.out.println(a);
 	}
 }
